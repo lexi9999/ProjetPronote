de31c6a95cef77ad73c531f74cf10ebe829614d4
matla91 <matiraeata@gmail.com>
2024-06-04 09:34:45 +0200

    Ajout des classes Utilisateur, Enseignant, Eleve, Note. Ajout des fonctions d'ajout et modification de note.

---
+++ ENSISmart/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Utilisateur(models.Model):
+    prenom = models.CharField(max_length=50)
+    nom = models.CharField(max_length=50)
+    mail = models.EmailField(max_length=50)
+    password = models.CharField(max_length = 12)
+
+    def __str__(self):
+        return f"{self.prenom} {self.nom}"
+
+
+class Eleve(Utilisateur):
+    TP = models.CharField(max_length=4)
+    TD = models.CharField(max_length=4)
+
+
+class Enseignant(Utilisateur):
+    matiere = models.CharField(max_length=100)
+    class Meta:
+        permissions = [
+            ("edit_note", "Peut ajouter et modier des notes"),
+        ]
+
+class Note(models.Model):
+    eleve = models.ForeignKey(Eleve, on_delete=models.CASCADE)
+    enseignant = models.ForeignKey(Enseignant, on_delete=models.CASCADE)
+    matiere = models.CharField(max_length=100)
+    note = models.DecimalField(max_digits=5, decimal_places=2)
+    commentaire = models.TextField(blank=True)
+    coefficient = models.IntegerField(blank = False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.eleve} - {self.matiere} - {self.note}"
