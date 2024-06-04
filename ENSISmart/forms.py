960b6b0697f71b1323a274cfcc0a3d12dac2f81b
matla91 <matiraeata@gmail.com>
2024-06-04 11:36:14 +0200

    Correction du formulaire de note.

---
+++ ENSISmart/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Note
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['eleve', 'enseignant', 'matiere', 'note', 'coefficient', 'commentaire', 'date'] 
