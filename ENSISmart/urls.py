a6bafd0403a5edf1f84041eb0807d00a27df10c0
Harry <harry.esslinger@uha.fr>
2024-06-07 09:46:19 +0200

    fix login url

--- ENSISmart/urls.py
+++ ENSISmart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import signup_view, success_view, reset_password_view, error_view
 
 urlpatterns = [
-    path('login/', signup_view, name='login'),
+    path('', signup_view, name='login'),
     path('success/', success_view, name='success'), 
     path('reset-password/<str:token>/', reset_password_view, name='reset_password_view'),
     path('error/', error_view, name='error')
