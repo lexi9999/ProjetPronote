6aa5408d252d2ec851c2556d4dd00d1df265896f
Okouiri <oussama.kouiri@uha.fr>
2024-06-10 11:34:50 +0200

    a

--- User/management/commands/import_data.py
+++ User/management/commands/import_data.py
@@ -1,16 +1,28 @@
 import pandas as pd
 from django.core.management.base import BaseCommand
 from django.contrib.auth.hashers import make_password
-from User.models import Eleve  # Assurez-vous de remplacer 'User' par le nom réel de votre application
+from User.models import Eleve, Enseignant
 
 class Command(BaseCommand):
     help = 'Importer des données depuis un fichier CSV'
 
     def add_arguments(self, parser):
         parser.add_argument('csv_file', type=str, help='Le chemin vers le fichier CSV')
+        parser.add_argument('--eleves', action='store_true', help='Importer des élèves')
+        parser.add_argument('--enseignants', action='store_true', help='Importer des enseignants')
 
     def handle(self, *args, **kwargs):
         csv_file = kwargs['csv_file']
+        eleves = kwargs['eleves']
+        enseignants = kwargs['enseignants']
+
+        if not eleves and not enseignants:
+            self.stdout.write(self.style.ERROR("Veuillez spécifier si vous voulez importer des élèves (--eleves) ou des enseignants (--enseignants)."))
+            return
+        elif eleves and enseignants:
+            self.stdout.write(self.style.ERROR("Veuillez spécifier un seul type d'importation : élèves (--eleves) ou enseignants (--enseignants), pas les deux."))
+            return
+
         try:
             data = pd.read_csv(csv_file)
         except FileNotFoundError:
@@ -23,23 +35,23 @@ class Command(BaseCommand):
             self.stdout.write(self.style.ERROR("Erreur de parsing dans le fichier CSV."))
             return
 
+        if eleves:
+            model = Eleve
+        elif enseignants:
+            model = Enseignant
+
         for index, row in data.iterrows():
             try:
-                # Convertir les valeurs booléennes et gérer les valeurs manquantes
-                is_first_co_value = row['Premiere connexion']
-                if pd.isna(is_first_co_value):
-                    is_first_co_value = True
-
-                eleve = Eleve.objects.create(
+                obj = model.objects.create(
                     name=row['NOM'],
                     firstName=row['Prénom'],
                     email=row['email'],
                     password=make_password(row['password']),  # Hashage du mot de passe
-                    is_first_co=is_first_co_value,  # Convertir la valeur en booléen
-                    last_login=row['Derniere connexion']  # Assigner la valeur de date
+                    is_first_co=True if pd.isna(row['Premiere connexion']) else False,  # Convertir la valeur en booléen
+                    last_login=row['Derniere connexion'] if pd.notna(row['Derniere connexion']) else None  # Assigner la valeur de date
                 )
-                self.stdout.write(self.style.SUCCESS(f"Élève {eleve.email} importé avec succès."))
+                self.stdout.write(self.style.SUCCESS(f"{model.__name__} {obj.name} {obj.firstName} importé avec succès."))
             except Exception as e:
-                self.stdout.write(self.style.ERROR(f"Erreur lors de l'importation de l'élève à la ligne {index+1}: {e}"))
+                self.stdout.write(self.style.ERROR(f"Erreur lors de l'importation de {model.__name__} à la ligne {index+1}: {e}"))
 
-        self.stdout.write(self.style.SUCCESS('Importation des données terminée avec succès'))
+        self.stdout.write(self.style.SUCCESS(f'Importation des données terminée avec succès'))
--- User/models.py
+++ User/models.py
@@ -14,7 +14,7 @@ class Eleve(models.Model):
        
 
     def __str__(self):
-        return "Eleve: " +  self.email
+        return "Eleve: " +  self.name + " " + self.firstName
     
 class Enseignant(models.Model):
     id = models.AutoField(primary_key=True)
@@ -26,7 +26,7 @@ class Enseignant(models.Model):
     last_login = models.DateTimeField(auto_now_add=True, null=True)
     
     def __str__(self):
-        return "Enseignant: " + self.email
+        return "Enseignant: " + self.name +" "+ self.firstName
 
 class Administrateur(models.Model):
     id = models.AutoField(primary_key=True)
