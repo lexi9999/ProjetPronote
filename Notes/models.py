999aae6eba91d653544045cb5daeb91eb2df01af
Harry <harry.esslinger@uha.fr>
2024-06-10 11:43:41 +0200

    ajout gestion notes

--- Notes/models.py
+++ Notes/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-
 from User.models import Eleve, Enseignant
 
+
+class Semestre(models.Model):
+    name=models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+    
 
 class UE(models.Model):
     name = models.CharField(max_length=255)
     coefficient = models.FloatField()
+    semestre= models.ForeignKey(Semestre, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
@@ -19,7 +26,7 @@ class Matiere(models.Model):
     
     def __str__(self):
         return self.name
-    
+
 
 class Note(models.Model):
     note = models.FloatField()
@@ -29,7 +36,4 @@ class Note(models.Model):
 
     def __str__(self):
         return self.note
-    
-
-
     
