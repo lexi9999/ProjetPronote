ed005ddddb4472a733c7ddc70fef7b4958466b9d
Okouiri <oussama.kouiri@uha.fr>
2024-06-10 10:10:47 +0200

    a

--- User/management/commands/import_data.py
+++ User/management/commands/import_data.py
@@ -1,24 +1,45 @@
 import pandas as pd
 from django.core.management.base import BaseCommand
-from models import Eleve  # Importer Eleve depuis le fichier models.py dans le même répertoire
+from django.contrib.auth.hashers import make_password
+from User.models import Eleve  # Assurez-vous de remplacer 'User' par le nom réel de votre application
 
 class Command(BaseCommand):
-    help = 'Import data from a CSV file'
+    help = 'Importer des données depuis un fichier CSV'
 
     def add_arguments(self, parser):
-        parser.add_argument('csv_file', type=str, help='The path to the CSV file')
+        parser.add_argument('csv_file', type=str, help='Le chemin vers le fichier CSV')
 
     def handle(self, *args, **kwargs):
         csv_file = kwargs['csv_file']
-        data = pd.read_csv(csv_file)
+        try:
+            data = pd.read_csv(csv_file)
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR(f"Le fichier {csv_file} n'a pas été trouvé."))
+            return
+        except pd.errors.EmptyDataError:
+            self.stdout.write(self.style.ERROR("Le fichier est vide."))
+            return
+        except pd.errors.ParserError:
+            self.stdout.write(self.style.ERROR("Erreur de parsing dans le fichier CSV."))
+            return
 
         for index, row in data.iterrows():
-            Eleve.objects.create(
-                name=row['NOM'],
-                firstName=row['Prénom'],
-                email=row['email'],
-                password=row['password'],
-                is_first_co=row['role']
-            )
+            try:
+                # Convertir les valeurs booléennes et gérer les valeurs manquantes
+                is_first_co_value = row['Premiere connexion']
+                if pd.isna(is_first_co_value):
+                    is_first_co_value = True
 
-        self.stdout.write(self.style.SUCCESS('Data imported successfully'))
+                eleve = Eleve.objects.create(
+                    name=row['NOM'],
+                    firstName=row['Prénom'],
+                    email=row['email'],
+                    password=make_password(row['password']),  # Hashage du mot de passe
+                    is_first_co=is_first_co_value,  # Convertir la valeur en booléen
+                    last_login=row['Derniere connexion']  # Assigner la valeur de date
+                )
+                self.stdout.write(self.style.SUCCESS(f"Élève {eleve.email} importé avec succès."))
+            except Exception as e:
+                self.stdout.write(self.style.ERROR(f"Erreur lors de l'importation de l'élève à la ligne {index+1}: {e}"))
+
+        self.stdout.write(self.style.SUCCESS('Importation des données terminée avec succès'))
--- User/models.py
+++ User/models.py
@@ -4,34 +4,37 @@ from datetime import timedelta
 from django.contrib.auth.hashers import make_password
 
 class Eleve(models.Model):
+    id = models.AutoField(primary_key=True) 
     name = models.CharField(max_length=255, unique=True)
     firstName = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255, null=True)
     is_first_co = models.BooleanField(default=True)
     last_login = models.DateTimeField(auto_now_add=True, null=True)
-    id = models.AutoField(primary_key=True, default=0)    
+       
 
     def __str__(self):
         return "Eleve: " +  self.email
     
 class Enseignant(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     firstName = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255, null=True)
     is_first_co = models.BooleanField(default=True)
     last_login = models.DateTimeField(auto_now_add=True, null=True)
-    id = models.AutoField(primary_key=True, default=0)
+    
     def __str__(self):
         return "Enseignant: " + self.email
 
 class Administrateur(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     firstName = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255, null=True)
     last_login = models.DateTimeField(auto_now_add=True, null=True)
-    id = models.AutoField(primary_key=True, default=0)
+    
     def __str__(self):
         return self.email
