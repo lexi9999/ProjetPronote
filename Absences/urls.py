4dbf8a6d56db4559ec97b229eb14c9efaa58c5da
Harry <harry.esslinger@uha.fr>
2024-06-10 11:26:08 +0200

    fix calendar

--- Absences/urls.py
+++ Absences/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from Absences import views
-from .views import UploadICSView, UploadICalendarLinkView, DeleteAllEventsView, AbsenceView
+from Absences.views import UploadICSView, UploadICalendarLinkView, DeleteAllEventsView, AbsenceView
 
 urlpatterns = [
     
