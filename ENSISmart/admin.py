1fe867c7a7af0e6d060007b70364eb1ac9795e0c
Okouiri <oussama.kouiri@uha.fr>
2024-06-11 15:26:51 +0200

    a

--- ENSISmart/admin.py
+++ ENSISmart/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from User.models import Eleve, Enseignant
-from ENSISmart.models import Eleve2
 
 admin.site.register(Eleve)
 admin.site.register(Enseignant)
-admin.site.register(Eleve2)
+
